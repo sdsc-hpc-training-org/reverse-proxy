3ffdba3d709ab0934f15210106315a50a3f80272
James Ian McDougall <jamesmcd@comet-ln2.sdsc.edu>
2020-04-28 09:57:07 -0700

    update default scripting

--- notebook.sh
+++ notebook.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
 # Argument 1 should be the Reverse Proxy API token
-# Argument 2 should be the tmpfile path
+# Argument 2 should be the starting directory
+# Argument 3 should be the tmpfile path
+# Argument 4 should be the time allotment
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH -p debug
-#SBATCH -o /dev/null
+#SBATCH -p compute
 #SBATCH --wait 0
 
-#echo "PARAM 1: $1"
-#echo "PARAM 2: $2"
-
-# DO NOT EDIT BELOW THIS LINE
-
 API_TOKEN=$1
-TMPFILE=$2
+TMPFILE=$3
 # Get the comet node's IP
 IP="$(hostname -s).local"
-#echo "Tempfile: $TMPFILE"
 jupyter notebook --ip $IP --config "$TMPFILE".py | tee $TMPFILE &
 
 # Waits for the notebook to start and gets the port
@@ -29,6 +24,8 @@ do
     PORT=${PORT#*".local:"}
     PORT=${PORT:0:4}
 done
+
+echo $PORT | tee -a $TMPFILE
 
 # redeem the API_TOKEN given the untaken port
 url='"https://manage.comet-user-content.sdsc.edu/redeemtoken.cgi?token=$API_TOKEN&port=$PORT"'
