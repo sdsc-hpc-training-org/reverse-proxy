ce569ed6ad1275963d99dabf6ebf0a775c96a4e8
James Ian McDougall <jamesmcd@comet-ln3.sdsc.edu>
2020-06-01 11:38:49 -0700

    Updated start notebook and batch/batch_notebook.sh

--- batch/batch_notebook.sh
+++ batch/batch_notebook.sh
@@ -6,38 +6,51 @@
 ## submit this batch script. You should never submit this batch script on
 ## its own, e.g. `sbatch batch_notebook.sh`. Don't do that :). You can
 ## specify this particluar batch script by using the -b flag, e.g.
-## ./start_notebook -b batch/batch_jupyterlab.sh
+## ./start_notebook.sh -b batch/batch_notebook.sh
 ## ======================================================================
 
+## This function takes  one parameter, the PID of the jupyter notebook process
+## The function returns the port which that jupyter notebook is running on.
+function get_jupyter_port() {
+    PID=$1
+    GREP_OUT=""
+    while [[ -z $GREP_OUT ]]; do
+        sleep 1
+        PORT_REGEX='^\s*\"port\":\s*\d+,$'
+        JUP_PATH=$(jupyter --runtime-dir)
+        GREP_OUT=$(grep -P $PORT_REGEX $(grep -lr $PID $JUP_PATH))
+    done
+    PORT=${GREP_OUT#*'"port":'}
+    PORT=${PORT:0:5}
+    echo $PORT
+}
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH -p compute
 #SBATCH --wait 0
 
 # DO NOT EDIT BELOW THIS LINE
 
-API_TOKEN=$1
-TMPFILE=$2
-# Get the comet node's IP
+# These variables are passed into the environment by `start_notebook`
+echo "RPS token: $api_token"
+echo "Config path: $config"
 
+# Get the comet node's IP (really just the hostname)
 IP="$(hostname -s).local"
-jupyter notebook --ip $IP --config "$TMPFILE".py | tee $TMPFILE &
+jupyter notebook --ip $IP --config $config --no-browser &
 
-# Waits for the notebook to start and gets the port
-PORT=""
-while [ -z "$PORT" ]
-do
-    PORT=$(grep '1\.' $TMPFILE)
-    PORT=${PORT#*".local:"}
-    PORT=${PORT:0:4}
-done
+# the last pid is stored in this variable
+JUPYTER_PID=$!
+PORT=$(get_jupyter_port $JUPYTER_PID)
 
-# redeem the API_TOKEN given the untaken port
-url='"https://manage.comet-user-content.sdsc.edu/redeemtoken.cgi?token=$API_TOKEN&port=$PORT"'
+# redeem the api_token given the untaken port
+url='"https://manage.comet-user-content.sdsc.edu/redeemtoken.cgi?token=$api_token&port=$PORT"'
 
-# Redeem the API_TOKEN
-eval curl $url | tee -a $TMPFILE
+# Redeem the api_token
+eval curl $url
+
+# try to remove the config file.
+rm $config
 
 # waits for all child processes to complete, which means it waits for the jupyter notebook to be terminated
 wait
--- tests/basic_jupyterlab.sh
+++ tests/basic_jupyterlab.sh
@@ -20,24 +20,13 @@ TMPFILE=$2
 # DO NOT EDIT BELOW THIS LINE
 # Get the comet node's IP
 IP="$(hostname -s).local"
-jupyter lab --ip $IP --config "$TMPFILE".py | tee $TMPFILE &
-
-# Waits for the notebook to start and gets the port
-PORT=""
-while [ -z "$PORT" ]
-do
-    PORT=$(grep '1\.' $TMPFILE)
-    PORT=${PORT#*".local:"}
-    PORT=${PORT:0:4}
-done
-
-echo $PORT | tee -a $TMPFILE
+jupyter lab --ip $IP --config "$TMPFILE".py &
 
 # redeem the API_TOKEN given the untaken port
 url='"https://manage.comet-user-content.sdsc.edu/redeemtoken.cgi?token=$API_TOKEN&port=$PORT"'
 
 # Redeem the API_TOKEN
-eval curl $url | tee -a $TMPFILE
+eval curl $url
 
 # waits for all child processes to complete, which means it waits for the jupyter notebook to be terminated
 wait
