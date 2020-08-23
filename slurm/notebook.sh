fc3799f741495421bfefa66bd39f43c3c30f8b97
James Ian McDougall <jamesmcd@comet-ln2.sdsc.edu>
2020-08-22 21:50:33 -0700

    Updated by adding lib

--- slurm/notebook.sh
+++ slurm/notebook.sh
@@ -13,24 +13,10 @@
 ## anything you gave to the start_notebook script like the time, partition, etc
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
+#SBATCH --wait=0
 
 # DO NOT EDIT BELOW THIS LINE
-
-## This function takes  one parameter, the PID of the jupyter notebook process
-## The function returns the port which that jupyter notebook is running on.
-function get_jupyter_port() {
-    PID=$1
-    GREP_OUT=""
-    while [[ -z $GREP_OUT ]]; do
-        sleep 1
-        PORT_REGEX='^\s*\"port\":\s*\d+,$'
-        JUP_PATH=$(jupyter --runtime-dir)
-        GREP_OUT=$(grep -P $PORT_REGEX $(grep -lr $PID $JUP_PATH))
-    done
-    PORT=${GREP_OUT#*'"port":'}
-    PORT=${PORT:0:5}
-    echo $PORT
-}
+source $start_root/lib/get_jupyter_port.sh
 
 # Get the comet node's IP (really just the hostname)
 IP="$(hostname -s).local"
